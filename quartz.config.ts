3c8f95b7bfd78390100659f56473eed9073fe62d
caijianfeng <cai.jianfeng@foxmail.com>
2024-02-21 18:46:26 +0800

    Quartz sync: Feb 21, 2024, 6:46 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
